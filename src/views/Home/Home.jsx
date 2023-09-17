488f12cf366654ae53e290b3f91b8bf28b966154
Marcos Monzón <marcos.monzon.iparraguirre.ext@ing.com>
2023-09-17 23:43:19 +0200

    update home and README

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,13 @@
 const Home = () => {
   return (
     <div className="Home">
-      <h1>Home</h1>
+      <h1 className="text-center mt-5">Gymhack Homepage</h1>
+      <img
+        src="https://logomaker.designfreelogoonline.com/media/productdesigner/logo/resized/1199_gym_logo-05.png"
+        alt="gymhack logo"
+        width="200"
+        className="round mx-auto d-block"
+      />
     </div>
   );
 }
